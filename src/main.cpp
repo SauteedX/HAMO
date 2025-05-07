93a195c632954b209daf1014bfbcc7782d217a16
Sautéed. <118161684+SauteedX@users.noreply.github.com>
2025-05-07 11:11:24 +0900

    structure updt

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,7 @@ void errorHandle(const char* errorMessage) {
 
 bool checkSensors() {
     // 각 센서의 값을 읽어서 유효한 범위인지 확인
+    // 센서 추가시 체크함수에 추가 필수!
     int prValue = analogRead(PR_PIN);
     int tmValue = analogRead(TM_PIN);
     int lmValue = analogRead(LM_PIN);
@@ -46,6 +47,7 @@ void setup() {
     Serial.println("Init Start!");
     lcd.init();
     lcd.backlight();
+    lcd.setCursor(0, 0);
     lcd.print("HAMO INIT...");
     
     // 센서들의 실제 값을 확인
@@ -53,14 +55,17 @@ void setup() {
         errorHandle("Sensor Error");
         return;
     }
-
-    lcd.clear();
     lcd.setCursor(0, 1);
     lcd.print("OK!");
-
+    delay(2000);
+    lcd.clear();
 }
 
 void loop() {
+    //Sensor Read
+    //Sensor Value Process
+    //H.W. Control
+    //APP output
 }
 //Compile Success.
 
